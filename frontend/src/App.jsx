c98a401959702652acbe207e25b0e3e4d19dc2d2
lazarodamasceno <lazarolawrence2@gmail.com>
2023-02-27 20:55:36 -0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Registration of Services</h1>
+      <form>
+          <div className="col-6">
+              <div>
+                  <label className="form-label">Name of the client: </label>
+                  <input type="text" className="form-control"/>
+              </div>
+              <div>
+                  <label className="form-label">Starting date: </label>
+                  <input type="date" className="form-control"/>
+              </div>
+              <div>
+                  <label className="form-label">Ending date: </label>
+                  <input type="date" className="form-control"/>
+              </div>
+          </div>
+      </form>
     </div>
   )
 }
