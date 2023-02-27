cae33416a8ae0665230c502f750804ce25ec4d0b
lazarodamasceno <lazarolawrence2@gmail.com>
2023-02-27 03:11:03 -0300

    commit

--- backend/src/main/java/com/backend/ServiceEntity.java
+++ backend/src/main/java/com/backend/ServiceEntity.java
@@ -1,4 +1,28 @@
 package com.backend;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "SERVICE")
+@Data
 public class ServiceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private int id;
+    @Column(nullable = false)
+    private String recipient;
+    @Column(nullable = false)
+    private double price;
+    @Column(nullable = false)
+    private String description;
+    private double pricePaid;
+    private Date commenceData;
+    private Date endingDate;
+    private Date paymentDate;
+
 }
--- backend/src/main/java/com/backend/ServiceRepository.java
+++ backend/src/main/java/com/backend/ServiceRepository.java
@@ -3,6 +3,6 @@ package com.backend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServiceRepository extends JpaRepository<ServiceEntity, Integer> {
-    ServiceEntity findById();
-    void deleteBy();
+    ServiceEntity findById(int id);
+    void deleteById(int id);
 }
