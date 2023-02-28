e3439ec853b1a23707c324085cfece49545ff9cf
lazarodamasceno <lazarolawrence2@gmail.com>
2023-02-28 20:23:36 -0300

    commit

--- frontend/src/Service.jsx
+++ frontend/src/Service.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Service() {
     return (
         <div className="container">
-
+            <div className="col-6">
+                <div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
