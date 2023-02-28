2ba82bcf3dfdbc7ef007d63bcb46f22878dd9a84
lazarodamasceno <lazarolawrence2@gmail.com>
2023-02-28 20:33:52 -0300

    commit

--- frontend/src/Service.jsx
+++ frontend/src/Service.jsx
@@ -5,7 +5,32 @@ export default function Service() {
         <div className="container">
             <div className="col-6">
                 <div>
-                    
+                    <label className="form-label">Client's name:</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div>
+                    <label className="form-label">Starting Date:</label>
+                    <input type="date" className="form-control" />
+                </div>
+                <div>
+                    <label className="form-label">Ending date:</label>
+                    <input type="date" className="form-control" />
+                </div>
+                <div>
+                    <label className="form-label">Description:</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div>
+                    <label className="form-label">Price:</label>
+                    <input type="number" className="form-control" />
+                </div>
+                <div>
+                    <label className="form-label">Price paid:</label>
+                    <input type="number" className="form-control" />
+                </div>
+                <div>
+                    <label className="form-label">Payment date:</label>
+                    <input type="date" className="form-control" />
                 </div>
             </div>
         </div>
