03e280e908d46093e216bbbceae22baf89434d53
lazarodamasceno <lazarolawrence2@gmail.com>
2023-02-27 21:10:46 -0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,7 @@
 function App() {
   return (
-    <div className="container">
-      <h1>Registration of Services</h1>
-      <form>
-          <div className="col-6">
-              <div>
-                  <label className="form-label">Name of the client: </label>
-                  <input name="recipient" type="text" className="form-control"/>
-              </div>
-              <div>
-                  <label className="form-label">Starting date: </label>
-                  <input name="startingDate" type="date" className="form-control"/>
-              </div>
-              <div>
-                  <label className="form-label">Ending date: </label>
-                  <input name="endingDate" type="date" className="form-control"/>
-              </div>
-              <div>
-                  <label className="form-label">Description: </label>
-                  <input name="description" type="text" className="form-control"/>
-              </div>
-              <div>
-                  <label className="form-label">Price: </label>
-                  <input name="price" type="text" className="form-control"/>
-              </div>
-              <div>
-                  <label className="form-label">Price paid: </label>
-                  <input name="pricePaid" type="number" className="form-control"/>
-              </div>
-              <div>
-                  <label className="form-label">Payment Date: </label>
-                  <input name="paymentDate" type="date" className="form-control"/>
-              </div>
-          </div>
-      </form>
+    <div>
+
     </div>
   )
 }
