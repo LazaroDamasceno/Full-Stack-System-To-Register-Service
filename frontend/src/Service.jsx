98479bff41bf2edda8b2f73a4650695a61878086
lazarodamasceno <lazarolawrence2@gmail.com>
2023-02-27 21:28:51 -0300

    commit

--- frontend/src/Service.jsx
+++ frontend/src/Service.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {useState} from "react";
 import {useEffect} from "react";
 
-export default function Service()
+export default function Service() {
 
     const [service, setService] = useState({
         recipient:'',
@@ -23,31 +23,31 @@ export default function Service()
                 <div className="col-6">
                     <div>
                         <label className="form-label">Client's name: </label>
-                        <input name="recipient" type="text" className="form-control"/>
+                        <input value={service.recipient} name="recipient" type="text" className="form-control"/>
                     </div>
                     <div>
                         <label className="form-label">Starting date: </label>
-                        <input name="startingDate" type="date" className="form-control"/>
+                        <input value={service.startingDate} name="startingDate" type="date" className="form-control"/>
                     </div>
                     <div>
                         <label className="form-label">Ending date: </label>
-                        <input name="endingDate" type="date" className="form-control"/>
+                        <input value={service.endingDate} name="endingDate" type="date" className="form-control"/>
                     </div>
                     <div>
                         <label className="form-label">Description: </label>
-                        <input name="description" type="text" className="form-control"/>
+                        <input value={service.description} name="description" type="text" className="form-control"/>
                     </div>
                     <div>
                         <label className="form-label">Price: </label>
-                        <input name="price" type="text" className="form-control"/>
+                        <input value={service.price} name="price" type="text" className="form-control"/>
                     </div>
                     <div>
                         <label className="form-label">Price paid: </label>
-                        <input name="pricePaid" type="number" className="form-control"/>
+                        <input value={service.pricePaid} name="pricePaid" type="number" className="form-control"/>
                     </div>
                     <div>
                         <label className="form-label">Payment Date: </label>
-                        <input name="paymentDate" type="date" className="form-control"/>
+                        <input value={service.paymentDate} name="paymentDate" type="date" className="form-control"/>
                     </div>
                 </div>
             </form>
