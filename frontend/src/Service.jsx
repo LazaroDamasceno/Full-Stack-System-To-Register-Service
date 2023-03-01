52ac38effecdb39f73dde7c24b282af8059d1c58
lazarodamasceno <lazarolawrence2@gmail.com>
2023-03-01 20:24:33 -0300

    commit

--- frontend/src/Service.jsx
+++ frontend/src/Service.jsx
@@ -12,6 +12,12 @@ export default function Service() {
         paymentDate:''
     })
 
+    const [services, setServices] = useState([])
+
+    function handleChange(event) {
+        setService({...service, [event.target.name]:[event.target.value]})
+    }
+
     return (
         <div className="container">
             <h1>Registration of Services</h1>
@@ -19,31 +25,31 @@ export default function Service() {
                 <div className="col-6">
                     <div>
                         <label className="form-label">Client's name:</label>
-                        <input name="clientName" type="text" className="form-control" />
+                        <input onChange={handleChange} value={service.clientName} name="clientName" type="text" className="form-control" />
                     </div>
                     <div>
                         <label className="form-label">Starting date:</label>
-                        <input name="startingDate"  type="date" className="form-control" />
+                        <input onChange={handleChange} value={service.startingDate}  name="startingDate"  type="date" className="form-control" />
                     </div>
                     <div>
                         <label className="form-label">Ending date:</label>
-                        <input name="endingDate"  type="date" className="form-control" />
+                        <input onChange={handleChange} value={service.endingDate}  name="endingDate"  type="date" className="form-control" />
                     </div>
                     <div>
                         <label className="form-label">Description:</label>
-                        <input name="description"  type="text" className="form-control" />
+                        <input onChange={handleChange} onChange={handleChange} value={service.description}  name="description"  type="text" className="form-control" />
                     </div>
                     <div>
                         <label className="form-label">Price:</label>
-                        <input name="price"  type="number" className="form-control" />
+                        <input onChange={handleChange} value={service.price}  name="price"  type="number" className="form-control" />
                     </div>
                     <div>
                         <label className="form-label">amount paid:</label>
-                        <input name="amountPaid"  type="number" className="form-control" />
+                        <input onChange={handleChange} value={service.amountPaid}  name="amountPaid"  type="number" className="form-control" />
                     </div>
                     <div>
                         <label className="form-label">Payment date:</label>
-                        <input name="paymentDate"  type="date" className="form-control" />
+                        <input onChange={handleChange} value={service.paymentDate}  name="paymentDate"  type="date" className="form-control" />
                     </div>
                 </div>
             </form>
