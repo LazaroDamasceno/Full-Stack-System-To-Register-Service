015f54b5bd680f890b803f9ff37492308e6da15a
lazarodamasceno <lazarolawrence2@gmail.com>
2023-03-02 01:14:17 -0300

    commit

--- frontend/src/Service.jsx
+++ frontend/src/Service.jsx
@@ -1,10 +1,60 @@
-import {useState} from "react";
-import axios from "axios";
-
 export default function Service() {
     return (
-        <div>
-
+        <div className="container">
+            <h1>Registration of services</h1>
+            <br />
+            <form>
+                <div className="col-6">
+                    <div>
+                        <label>client's name:</label>
+                        <input name="clientName"
+                               type="text"
+                               className="form-control"/>
+                    </div>
+                    <br />
+                    <div>
+                        <label>starting date:</label>
+                        <input  name="startingDate"
+                                type="date"
+                                className="form-control"/>
+                    </div>
+                    <br />
+                    <div>
+                        <label>ending date:</label>
+                        <input name="endingDate"
+                               type="date"
+                               className="form-control"/>
+                    </div>
+                    <br />
+                    <div>
+                        <label>description:</label>
+                        <input name="description"
+                               type="text"
+                               className="form-control"/>
+                    </div>
+                    <br />
+                    <div>
+                        <label>price:</label>
+                        <input name="price"
+                               type="number"
+                               className="form-control"/>
+                    </div>
+                    <br />
+                    <div>
+                        <label>amount paid:</label>
+                        <input name="amountPaid"
+                               type="number"
+                               className="form-control"/>
+                    </div>
+                    <br />
+                    <div>
+                        <label>payment day:</label>
+                        <input name="paymentDay"
+                               type="date"
+                               className="form-control"/>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
