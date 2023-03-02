657a3f243be37d443fcf135f55abc8318362cb2c
lazarodamasceno <lazarolawrence2@gmail.com>
2023-03-02 13:37:21 -0300

    commit

--- frontend/src/Service.jsx
+++ frontend/src/Service.jsx
@@ -30,81 +30,87 @@ export default function Service() {
         <div className="container">
             <h1>Registration of services</h1>
             <br />
-            <form onSubmit={handleSubmit}>
-                <div className="col-6">
-                    <div>
-                        <label>client's name:</label>
-                        <input name="clientName"
-                               type="text"
-                               className="form-control"
-                               value={service.clientName}
-                               onChange={handleChange}
-                        />
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <div className="col-6">
+                        <div>
+                            <label>client's name:</label>
+                            <input name="clientName"
+                                   type="text"
+                                   className="form-control"
+                                   value={service.clientName}
+                                   onChange={handleChange}
+                            />
+                        </div>
+                        <br />
+                        <div>
+                            <label>starting date:</label>
+                            <input  name="startingDate"
+                                    type="date"
+                                    className="form-control"
+                                    value={service.startingDate}
+                                    onChange={handleChange}
+                            />
+                        </div>
+                        <br />
+                        <div>
+                            <label>ending date:</label>
+                            <input name="endingDate"
+                                   type="date"
+                                   className="form-control"
+                                   value={service.endingDate}
+                                   onChange={handleChange}
+                            />
+                        </div>
+                        <br />
+                        <div>
+                            <label>description:</label>
+                            <input name="description"
+                                   type="text"
+                                   className="form-control"
+                                   value={service.description}
+                                   onChange={handleChange}
+                            />
+                        </div>
+                        <br />
+                        <div>
+                            <label>price:</label>
+                            <input name="price"
+                                   type="number"
+                                   className="form-control"
+                                   value={service.price}
+                                   onChange={handleChange}
+                            />
+                        </div>
+                        <br />
+                        <div>
+                            <label>amount paid:</label>
+                            <input name="amountPaid"
+                                   type="number"
+                                   className="form-control"
+                                   value={service.amountPaid}
+                                   onChange={handleChange}
+                            />
+                        </div>
+                        <br />
+                        <div>
+                            <label>payment day:</label>
+                            <input name="paymentDay"
+                                   type="date"
+                                   className="form-control"
+                                   value={service.paymentDay}
+                                   onChange={handleChange}
+                            />
+                        </div>
+                        <br />
+                        <input type="submit" value="Register" className="btn btn-success"/>
                     </div>
-                    <br />
-                    <div>
-                        <label>starting date:</label>
-                        <input  name="startingDate"
-                                type="date"
-                                className="form-control"
-                                value={service.startingDate}
-                                onChange={handleChange}
-                        />
-                    </div>
-                    <br />
-                    <div>
-                        <label>ending date:</label>
-                        <input name="endingDate"
-                               type="date"
-                               className="form-control"
-                               value={service.endingDate}
-                               onChange={handleChange}
-                        />
-                    </div>
-                    <br />
-                    <div>
-                        <label>description:</label>
-                        <input name="description"
-                               type="text"
-                               className="form-control"
-                               value={service.description}
-                               onChange={handleChange}
-                        />
-                    </div>
-                    <br />
-                    <div>
-                        <label>price:</label>
-                        <input name="price"
-                               type="number"
-                               className="form-control"
-                               value={service.price}
-                               onChange={handleChange}
-                        />
-                    </div>
-                    <br />
-                    <div>
-                        <label>amount paid:</label>
-                        <input name="amountPaid"
-                               type="number"
-                               className="form-control"
-                               value={service.amountPaid}
-                               onChange={handleChange}
-                        />
-                    </div>
-                    <br />
-                    <div>
-                        <label>payment day:</label>
-                        <input name="paymentDay"
-                               type="date"
-                               className="form-control"
-                               value={service.paymentDay}
-                               onChange={handleChange}
-                        />
-                    </div>
-                    <br />
-                    <input type="submit" value="Register" className="btn btn-success"/>
-                </div>
-            </form>
+                </form>
+            </section>
+            <br />
+            <section>
+
+            </section>
         </div>
     )
 }
